ecf2c81fbacb3b45a53346f02ee193bdddff2091
minakoki1 <minaarsic3@gmail.com>
2021-11-15 22:31:30 +0100

    Kreiran servis za rad sa proizvodima

--- server/index.php
+++ server/index.php
@@ -1,15 +1,18 @@
 <?php
 include "vrstaServis.php";
+include "proizvodServis.php";
 include "broker.php";
 class Controller{
 
     private $vrstaServis;
+    private $proizvodServis;
     private $broker;
     private static $controller;
 
     private function __construct(){
         $this->broker=new Broker();
         $this->vrstaServis=new VrstaServis($this->broker);
+        $this->proizvodServis=new ProizvodServis($this->broker);
     }
 
     public static function getController(){
@@ -31,6 +34,9 @@ class Controller{
         $operacija=$_GET["operacija"];
         $metoda=$_SERVER['REQUEST_METHOD'];
         if($operacija=='vratiVrste'){
+            if($metoda!=='GET'){
+                throw new Exception('Putanja nije pronadjena');
+            }
             return $this->vrstaServis->vratiSveVrste();
         }
         if($operacija=='kreirajVrstu'){
@@ -54,6 +60,33 @@ class Controller{
             $this->vrstaServis->obrisiVrstu($_POST['id']);
             return null;
         }
+        if($operacija=='vratiProizvode'){
+            if($metoda!=='GET'){
+                throw new Exception('Putanja nije pronadjena');
+            }
+            return $this->proizvodServis->vratiSveProizvode();
+        }
+        if($operacija=='kreirajProizvod'){
+            if($metoda!=='POST'){
+                throw new Exception('Putanja nije pronadjena');
+            }
+            $this->proizvodServis->kreirajProizvod($_POST['naziv']);
+            return null;
+        }
+        if($operacija=='izmeniProizvod'){
+            if($metoda!=='POST'){
+                throw new Exception('Putanja nije pronadjena');
+            }
+            $this->proizvodServis->izmeniProizvod($_POST['id'],$_POST['naziv']);
+            return null;
+        }
+        if($operacija=='obrisiProizvod'){
+            if($metoda!=='POST'){
+                throw new Exception('Putanja nije pronadjena');
+            }
+            $this->proizvodServis->obrisiProizvod($_POST['id']);
+            return null;
+        }
         throw new Exception('Operacija nije podrzana');
     }
 
@@ -67,7 +100,7 @@ class Controller{
             "status"=>true,
             "data"=>$podaci
         ];
-    }
+     }
      private function vratiGresku($greska){
         return[
             "status"=>false,
